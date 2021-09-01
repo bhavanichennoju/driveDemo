120610868fd3a596e16eba303110ff8805dc11c0
Bhavani Chennoju <bhavanic@nbos.vinisoft.com>
2021-09-01 13:48:39 +0530

    initial commit

---
+++ src/main/java/com/google/driveDemo/App.java
@@ -0,0 +1,17 @@
+package com.google.driveDemo;
+
+import org.apache.commons.lang3.text.WordUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(WordUtils.swapCase("bhavani"));
+        System.out.println( WordUtils.capitalizeFully("hello world" ));
+    }
+}
